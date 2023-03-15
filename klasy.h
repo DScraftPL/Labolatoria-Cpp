8a243f9b0765be30417137694853084563322ce5
DScraftPL <xxxwiacekxxx@gmail.com>
2023-03-15 16:58:25 +0100

    plzs

--- klasy.h
+++ klasy.h
@@ -1,5 +1,47 @@
 #ifndef KLASY_H
 #define KLASY_H
+#include <iostream>
+//po wywołaniu metody, powinna zawsze działać
+//
+
+class student
+{
+    public:
+        //konstruktor pozwala zachować zgodność danych
+        student(std::string, std::string, int, int, int);
+        student(const student&){}; // konstruktor kopioujacy pozwoli kopiować obiekty
+        //2 funkcje o tej samej nazwie, bo c++ ma przeciążanie nazwy POG
+        //nic nie stoi na przeszkodzie zapisania funkcji z inną syngnaturą (typ, lista arg)
+        float dochodNaOsobe()
+        {
+            return dochod/liczbaOsob;
+        }
+
+        //hermetyzacja zabronienie modyfikacji danych
+        //publiczne funkcjie dadzą możliwość zmiany danych prywatnych
+        int getLiczbaOsob() const
+        {
+            return liczbaOsob;
+        }
+        //modyfikator const - ta metoda nie modyfikujeniczego
+        //object będzie read only poprzez const
+        void setLiczbaOsob(int x)
+        {
+            if(x > 0)
+            {
+                liczbaOsob = x;
+            }
+        }
+        //ta metoda pozwoli ustawić liczbe
+    private:
+        std::string imie;
+        std::string nazwisko;
+        int wiek;
+        float dochod;
+        int liczbaOsob=1;
+        int *tab; //przy kopiowaniu s2 bedzie miało ten sam wskanik co s1 przez co mamy  2 obiekty
+        // które wskazują na tablice
+};
 
 class ZLab02
 {
--- programy/lab3.cpp
+++ programy/lab3.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 int main()
 {
-    
+    student s("Jan","Kowalski", 22, 32000, 4);
+    student s2 = s;
+    return 0;
 }
